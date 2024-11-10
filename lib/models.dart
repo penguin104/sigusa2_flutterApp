9b9bb0fdd37b451492d06b227deb1787438ff0f9
penguin104 <jd1908_0808@yahoo.co.jp>
2024-11-10 21:24:07 +0900

    モデルクラス実装

---
+++ lib/models.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+
+class ProgramModel {
+  String action;
+  String color;
+  ProgramModel({
+    required this.action,
+    required this.color,
+  });
+
+  ProgramModel.fromJson(Map<String, dynamic> json)
+      : action = json["action"],
+        color = json["color"];
+}
+
+class ProjectModel {
+  int project_id;
+  String project_name;
+  List<ProgramModel> program;
+  ProjectModel({
+    required this.project_id,
+    required this.project_name,
+    required this.program,
+  });
+
+  ProjectModel.fromJson(Map<String, dynamic> json)
+      : project_id = json["project_id"],
+        project_name = json["project_name"],
+        program = (jsonDecode(json["program"]) as List)
+            .map((e) => ProgramModel.fromJson(e))
+            .toList();
+}
